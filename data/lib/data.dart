7ce619c1e337aaabf079ef537c61c0434fd4bad9
Valerio Leite <valeriobleite@gmail.com>
2021-04-02 22:26:20 +0100

    First commit

---
+++ data/lib/data.dart
@@ -0,0 +1,11 @@
+library data;
+
+/// A Calculator.
+class Data {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  static test() {
+    print("Data->test");
+  }
+}
---
+++ domain/lib/domain.dart
@@ -0,0 +1,11 @@
+library domain;
+
+/// A Calculator.
+class Domain {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  static test() {
+    print("Domain->test");
+  }
+}
